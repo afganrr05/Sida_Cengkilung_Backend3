928926d0fa1f1512ca8de3a1e7e618a8697894d9
jucodemanajemen-gif <jucode.manajemen@gmail.com>
2026-07-21 02:15:36 +0800

    Update konten.routes.js

--- src/routes/konten.routes.js
+++ src/routes/konten.routes.js
@@ -5,15 +5,15 @@ const authMiddleware = require('../middlewares/auth.middleware');
 const roleMiddleware = require('../middlewares/role.middleware');
 const { upload } = require('../middlewares/upload.middleware');
 
-// Public routes - bisa diakses guest
-router.get('/', kontenController.getAllKonten);
-router.get('/kategori', kontenController.getKategoriKonten);
-router.get('/:id', kontenController.getKontenById);
-
 // Admin routes
 router.get('/admin/semua', authMiddleware, roleMiddleware('admin'), kontenController.getAllKontenAdmin);
 router.post('/', authMiddleware, roleMiddleware('admin'), upload.single('thumbnail'), kontenController.createKonten);
 router.put('/:id', authMiddleware, roleMiddleware('admin'), upload.single('thumbnail'), kontenController.updateKonten);
 router.delete('/:id', authMiddleware, roleMiddleware('admin'), kontenController.deleteKonten);
+
+// Public routes - bisa diakses guest
+router.get('/', kontenController.getAllKonten);
+router.get('/kategori', kontenController.getKategoriKonten);
+router.get('/:id', kontenController.getKontenById);
 
 module.exports = router;
